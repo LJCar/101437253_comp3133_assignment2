4093d5874f0c1e33498e348b02279ce71326f08c
LJCar <ljcaridi@yahoo.com>
2025-04-03 23:47:35 -0400

    Added route protection on employee component linked with a logout button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { EmployeeComponent } from './employee/employee.component';
+import {authGuard} from './guards/auth.guard';
+import {LogoutComponent} from './logout/logout.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'employees', component: EmployeeComponent }
+  { path: 'employees', component: EmployeeComponent, canActivate: [authGuard] },
+  { path: 'logout', component: LogoutComponent }
 ];
